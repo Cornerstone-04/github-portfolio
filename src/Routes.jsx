487318de0b544e80590afcc696d45749c0042eac
Cornerstone Ephraim <fortunecornerstone@gmail.com>
2023-10-17 21:51:02 +0100

    feat: add error boundary

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { NotFound, Repositories, Repository } from "./pages";
+import { ErrorBoundary, NotFound, Repositories, Repository } from "./pages";
 const AppRoutes = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" exact element={<Repositories />} />
-        <Route path="/repository/:repoName" element={<Repository />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <ErrorBoundary>
+        <Routes>
+          <Route path="/" exact element={<Repositories />} />
+          <Route path="/repository/:repoName" element={<Repository />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </ErrorBoundary>
     </Router>
   );
 };
